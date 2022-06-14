e61a3d49adb8c4a95c26b9b36a3b071f297a9fa3
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-06-14 10:44:54 -0300

    trying routes and controller - not finished

---
+++ API/src/controllers/userController.js
@@ -0,0 +1,23 @@
+const { Users } = require("../database/models");
+
+const userController = {
+	async create(req, res) {
+		try {
+			res.status(201);
+		} catch (error) {
+			console.log(error);
+			return res.status(201);
+		}
+	},
+
+	async getAll(req, res) {
+		try {
+			res.status(201);
+		} catch (error) {
+			console.log(error);
+			return res.status(201);
+		}
+	}
+};
+
+module.exports = userController;
