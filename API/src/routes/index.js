f94b5c414c52fcff4bba7b718ffe16dc4856a3ae
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-06-14 19:23:46 -0300

    all routes

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -6,6 +6,18 @@ routes.get("/", (req, res) => {
 	res.status(200).json({ msg: "teste de rotas" });
 });
 
-routes.post("/usuarios", UserController.create);
+routes.post("/usuario/criar", UserController.create);
+routes.get("/usuario/listar");
+routes.put("/usuario/atualizar/:id");
+routes.delete("/usuario/apagar/:id");
+
+routes.post("/post/criar");
+routes.get("/post");
+routes.put("/post/atualizar/:id");
+routes.delete("/post/apagar/:id");
+
+routes.get("/", (req, res) => {
+	res.status(200).json({ msg: "tesdte rotas" });
+});
 
 module.exports = routes;
