bab825419a7f1607589451536b905548cec83470
Gabriela de Valnisio <gabi.valnisio23@gmail.com>
2022-06-14 17:15:59 -0300

    iniciando o vite

---
+++ WEB/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Header from "../components/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
