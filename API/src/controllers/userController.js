9d0af557cc562f0761fa060c4323d7fa14c94ce9
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-14 20:28:29 -0300

    add user controller

--- API/src/controllers/userController.js
+++ API/src/controllers/userController.js
@@ -1,12 +1,34 @@
 const { Users } = require("../database/models");
 
 const UserController = {
-	async create(req, res) {
-		const newUser = await Users.create({
-			...req.body
-		});
-		return res.status(201).json(newUser);
-	}
+
+	// async create(req, res) {
+	// 	const newUser = await Users.create({
+	// 		...req.body
+	// 	});
+	// 	return res.status(201).json(newUser);
+	// },
+
+	
+	async listar(req, res) {
+		try {
+			const allUsers = await Users.findAll({
+				attributes: {exclude:"password"}
+				
+			});
+
+			console.log(allUsers);
+
+			if (!allUsers){
+				return res.status(200).json("Nenhum usuário cadastrado!")
+			}
+
+			res.status(200).json(allUsers);
+		}
+		catch (error) {
+			return res.status(500).json("Ocorreu um erro ao listar usuários");
+		};
+	},
 };
 
 module.exports = UserController;
