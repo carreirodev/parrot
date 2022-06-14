4f8a92e30211ac2d12e78b819ff3747fdc0d9abf
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-14 20:30:22 -0300

    add controllers on routes

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -1,23 +1,20 @@
 const express = require("express"),
 	routes = express.Router(),
-	UserController = require("../controllers/userController");
+	UserController = require("../controllers/userController"), PostsController = require("../controllers/postsController")
 
 routes.get("/", (req, res) => {
 	res.status(200).json({ msg: "teste de rotas" });
 });
 
-routes.post("/usuario/criar", UserController.create);
-routes.get("/usuario/listar");
+routes.post("/usuario/criar");
+routes.get("/usuario/listar", UserController.listar);
 routes.put("/usuario/atualizar/:id");
 routes.delete("/usuario/apagar/:id");
 
 routes.post("/post/criar");
-routes.get("/post");
+routes.get("/post/listar", PostsController.listar);
 routes.put("/post/atualizar/:id");
 routes.delete("/post/apagar/:id");
 
-routes.get("/", (req, res) => {
-	res.status(200).json({ msg: "tesdte rotas" });
-});
 
 module.exports = routes;
