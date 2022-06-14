67519507868b6eca822e9bb4521a6ad822228f44
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-14 19:44:22 -0300

    fix imports routes and add cors

--- API/src/app.js
+++ API/src/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const routes = require("./routes");
 const db = require("./database");
 const cors = require("cors");
-const routes = express.Router();
 
 const app = express();
 
+app.use(cors)
+
 app.use(express.json());
-app.use(routes);
 
 app.use(routes);
 
