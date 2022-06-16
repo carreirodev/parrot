794b37a7ae7da204612c922bcff19ee3d3393990
karumundo <karumundo@gmail.com>
2022-06-16 19:16:53 -0300

    Removendo node_modules

--- API/src/database/config/config.js
+++ API/src/database/config/config.js
@@ -1,23 +1,23 @@
-require("dotenv").config();
+ require("dotenv").config();
 
 module.exports = {
 	development: {
 		username: "root",
-		password: "Hardc0reMac",
+		password: "sdGyJJhChiqbydu8VCut",
 		database: "parrot_social",
 		host: "127.0.0.1",
 		dialect: "mysql"
 	},
 	test: {
 		username: "root",
-		password: "Hardc0reMac",
+		password: "sdGyJJhChiqbydu8VCut",
 		database: "parrot_social",
 		host: "127.0.0.1",
 		dialect: "mysql"
 	},
 	production: {
 		username: "root",
-		password: "Hardc0reMac",
+		password: "sdGyJJhChiqbydu8VCut",
 		database: "parrot_social",
 		host: "127.0.0.1",
 		dialect: "mysql"
