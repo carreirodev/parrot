0b55337c48afd7da2058e334dc7acbc6ec769df0
andersonmsousa <anderson.msousa13@gmail.com>
2022-06-16 19:14:51 -0300

    adicionando componente user e ajustes

--- WEB/src/pages/Feed/index.tsx
+++ WEB/src/pages/Feed/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header"
+import User from "../../components/User"
 
 const Feed: React.FC = () => {
 	return (
 		<div className="Feed">
 			<Header />
+			<User />
 		</div>
 	);
 };
--- WEB/src/services/MainApi/config/index.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const baseAPi = axios.create({
-    baseURL: 'http://localhost:4000',
-});
-
-export default baseAPi;
