5f76519b2412d0e0eda39c01e04ef44265507624
Gabriela de Valnisio <gabi.valnisio23@gmail.com>
2022-06-16 14:24:44 -0300

    fazendo o componente "publicar post"

--- WEB/src/pages/Feed/index.tsx
+++ WEB/src/pages/Feed/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "../../components/Header"
+import Header from "../../components/Header";
+import Publicar from "../../components/Publicar";
 
 const Feed: React.FC = () => {
 	return (
 		<div className="Feed">
 			<Header />
+			<Publicar />
 		</div>
 	);
 };
