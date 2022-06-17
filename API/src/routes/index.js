efb5679b04ec2f819eca0cf4e475069560f6de04
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-17 11:58:06 -0300

    fix imports

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -1,14 +1,20 @@
-const { application } = require("express");
 
 const express = require("express"),
-	routes = express.Router(),
+routes = express.Router(),
 UserController = require("../controllers/userController"),
-PostController = require("../controllers/postController");
+PostController = require("../controllers/postController"),
+AuthController = require("../controllers/authController"),
+usersCreateValidation = require("../validator/users/create"),
+usersUpdateValidation = require("../validator/users/update"),
+postsCreateValidation = require("../validator/posts/create"),
+postsUpdateValidation = require("../validator/posts/update"),
+authLoginValidation = require("../validator/auth/login");
 
-routes.post("/usuario", UserController.create);
+const auth = require("../middlewares/auth");
+routes.post("/usuario", usersCreateValidation, UserController.create);
 routes.get("/usuario", UserController.listar);
-routes.patch("/usuario/:id", UserController.alterar);
-routes.delete("/usuario/apagar/:id");
+routes.put("/usuario/:id", usersUpdateValidation, UserController.alterar);
+routes.delete("/usuario/:id");
 
 routes.post("/post", postsCreateValidation, PostController.create);
 routes.get("/post", PostController.listar);
